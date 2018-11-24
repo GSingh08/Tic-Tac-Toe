d00720e24724c16e52033dbac6d81758bf3797ba
Gurjot Singh <gurjotsingh@Gurjots-MBP.fios-router.home>
2018-11-24 15:51:38 -0500

    all logic done, but can't get Letters on screen

--- main.js
+++ main.js
@@ -25,19 +25,52 @@ function grabMessage(msg){
 }
 
 function nextMove(box){
-  if(box.innerText == ''){
+  if(box.innerText == "."){
   box.innerText = document.turn;
   turn();
   }else{
-    grabMessage("srry");
+    grabMessage("Sorry");
   }
 }
 
 function turn(){
-  if(document.turn == "X"){
+  if (checkForWinner(document.turn)) {
+    grabMessage("Congratulations " + document.turn + "!  You Win!");
+
+  }else if (document.turn == "X"){
     document.turn = "O";
+    grabMessage("It's Your Turn " + document.turn);
   }else{
     document.turn = "X";
+    grabMessage("It's Your Turn " + document.turn);
   }
-  grabMessage("It's Your Turn " + document.turn + ".")
+}
+
+function checkForWinner(move){
+  let result = false;
+  if (checkRow(1,2,3, move) || 
+      checkRow(4,5,6, move) ||
+      checkRow(7,8,9, move) || 
+      checkRow(1,4,7, move) || 
+      checkRow(2,5,8, move) || 
+      checkRow(3,6,9, move) || 
+      checkRow(1,5,9, move) || 
+      checkRow(3,5,7, move)) {
+    
+        result = true; 
+  }
+  return result;
+}
+
+
+function checkRow(a,b,c,move){
+  let result = false;
+  if(getBox(a) == move && getBox(b)== move && getBox(c)== move){
+    result = true;
+  }
+  return result;
+}
+
+function getBox(number){
+  return document.getElementById("s" + number).innerText;
 }
